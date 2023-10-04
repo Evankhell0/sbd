c9d5c1b1c46fe9644bc0d425be941c8b20575ed8
Evankhell <68199237+Evankhell0@users.noreply.github.com>
2023-10-04 19:31:36 +0200

    fetch secret count on dungeon start

--- index.js
+++ index.js
@@ -1,13 +1,24 @@
+import request from "requestV2"
+
+const config = {
+    "key": "apikey",
+    "uuid": "uuid"
+}
+
 register("chat", () => {
     ChatLib.chat("Entered Dungeon")
-}).setChatCriteria("You are not allowed to use Potion Effects while in Dungeon, therefore all active effects have been paused and stored. They will be restored when you leave Dungeon!");
+    let playerData = request({url: `https://api.hypixel.net/player?key=${config.key}&uuid=${config.uuid}`, json: true}).then(data => {
+        let secrets = data.player.achievements.skyblock_treasure_hunter
+        ChatLib.chat(`Starting secrets: ${secrets}`)
+    })
+}).setChatCriteria("You are not allowed to use Potion Effects while in Dungeon, therefore all active effects have been paused and stored. They will be restored when you leave Dungeon!")
 
 register("chat", (score, rank) => {
     let lastSlotItem = Player.getInventory().getStackInSlot(8)
-    if (lastSlotItem.getName().includes("Your Score Summary")) {
+    if(lastSlotItem?.getName()?.includes("Your Score Summary")) {
         ChatLib.chat("Completed Dungeon")
     }
-}).setChatCriteria("Team Score: ${score} (${rank})").setContains();
+}).setChatCriteria("Team Score: ${score} (${rank})").setContains()
 
 let partyMembers = []
 

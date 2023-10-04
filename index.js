00af5718d6121ff154462c40d2cd9d4d62a487b8
N1K0LAAAA <nikolapraktikum@gmail.com>
2023-10-04 18:28:53 +0200

    PArty members addding / removing

--- index.js
+++ index.js
@@ -3,8 +3,30 @@ register("chat", () => {
 }).setChatCriteria("You are not allowed to use Potion Effects while in Dungeon, therefore all active effects have been paused and stored. They will be restored when you leave Dungeon!");
 
 register("chat", (score, rank) => {
-    let lastSlotItem = Player.getInventory().getStackInSlot(8);
-    if(lastSlotItem.getName().includes("Your Score Summary")) {
-        ChatLib.chat("Completed Dungeon");
+    let lastSlotItem = Player.getInventory().getStackInSlot(8)
+    if (lastSlotItem.getName().includes("Your Score Summary")) {
+        ChatLib.chat("Completed Dungeon")
     }
 }).setChatCriteria("Team Score: ${score} (${rank})").setContains();
+
+let partyMembers = []
+
+register("chat", (user) => {
+    partyMembers.push(user)
+    ChatLib.chat(partyMembers.join(","))
+}).setChatCriteria("You have joined ${user} party!")
+
+register("chat", (users) => {
+    partyMembers.push(users)
+    ChatLib.chat(partyMembers.join(","))
+}).setChatCriteria("You'll be partying with: ${users}")
+
+register("chat", () => {
+    partyMembers = []
+    ChatLib.chat(partyMembers.join(","))
+}).setChatCriteria("You left the party.")
+
+register("chat", (user) => {
+    partyMembers =  partyMembers.filter(x => x != user)
+    ChatLib.chat(partyMembers.join(","))
+}).setChatCriteria("${user} has left the party.")

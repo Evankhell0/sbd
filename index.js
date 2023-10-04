e6eea3b656dc687681e2598bb8e04b834eadd17e
Evankhell <68199237+Evankhell0@users.noreply.github.com>
2023-10-04 17:58:50 +0200

    added chat criteria for dungeon start/end

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+register("chat", () => {
+    ChatLib.chat("Entered Dungeon")
+}).setChatCriteria("You are not allowed to use Potion Effects while in Dungeon, therefore all active effects have been paused and stored. They will be restored when you leave Dungeon!");
 
+register("chat", (score, rank) => {
+    let lastSlotItem = Player.getInventory().getStackInSlot(8);
+    if(lastSlotItem.getName().includes("Your Score Summary")) {
+        ChatLib.chat("Completed Dungeon");
+    }
+}).setChatCriteria("Team Score: ${score} (${rank})").setContains();

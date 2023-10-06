f07892d0531a85c3ad3bd3a6fc8c7bed85d79b7f
Evankhell <68199237+Evankhell0@users.noreply.github.com>
2023-10-06 12:10:11 +0200

    partymembers uuids are now taken from mojang api

--- util/partymember.js
+++ util/partymember.js
@@ -3,16 +3,18 @@ import request from "requestV2"
 
 class PartyMember {
     constructor(name) {
-        this.name = name;
+        this.name = name
         this.secrets = {
             old: 0,
             new: 0
         }
+        request({url: `https://api.mojang.com/users/profiles/minecraft/${name}`, json: true}).then(data => {
+            this.uuid = data.id
+        }).catch(e => console.log(`[SBD] Could not fiind uuid for ${this.name}: ${e}`))
     }
 
     updateSecrets() {
-        //TODO change uuid from config to actual party members
-        return request({url: `https://api.hypixel.net/player?key=${config.key}&uuid=${config.uuid}`, json: true}).then(data => {
+        return request({url: `https://api.hypixel.net/player?key=${config.key}&uuid=${this.uuid}`, json: true}).then(data => {
             this.secrets.old = this.secrets.new
             this.secrets.new = data.player.achievements.skyblock_treasure_hunter
             let difference = this.secrets.new - this.secrets.old

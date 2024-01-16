92e2c81618246682e6d860b7da862a2310165b62
Evankhell0 <68199237+Evankhell0@users.noreply.github.com>
2024-01-16 09:37:25 +0100

    show secret count in party finder

--- util/data.js
+++ util/data.js
@@ -1,7 +1,13 @@
 import { Party } from "./party.js"
+import { PartyMember } from "./partymember.js"
 
 class Data {
     //static party = new Party()
+    static players = {}
+
+    static addPlayer(name) {
+        this.players[name] = new PartyMember(name)
+    }
 }
 
 module.exports = { Data }
--- util/partyfinder.js
+++ util/partyfinder.js
@@ -1,21 +1,38 @@
+import request from "requestV2"
+import { Data } from "./data.js"
+
 const registerPartyFinderTriggers = () => {
     register("itemTooltip", (lore, item) => {
-        if(!/'s Party§r/.test(lore[0])) {
-            return;
-        }
+        const itemName = lore[0];
         lore = lore.slice(1);
         lore = lore.filter(x => !/minecraft:/.test(x) && !/NBT:/.test(x))
+
+        let hasChanged = false;
         lore = lore.map(x => {
-            if(/§5§o §\w\w+§f: §\w\w+§b \(§e\d+§b\)/.test(x)) {
-                const name = x.replace(/(§5§o §\w)|(§f: §\w\w+§b \(§e\d+§b\))/g, "")
-                // TODO
-                // get data from hypixel api and adjust return value accordingly
-                return `§b${name}`;
+
+            if(/§f\[§a\w+§f\]§r/.test(x)) {
+                const name = x.replace(/(.*§5§o §\w)|(§f: §\w\w+§b \(§e\d+§b\).*)/g, "")
+                const player = Data.players[name]
+                if(player?.uuid.new != player?.uuid.old || player?.secrets.new != player?.secrets.old) {
+                    hasChanged = true;
+                }
+                const noSuffix = x.replace(/ §f\[§a\w+§f\]§r/, "")
+                return `${noSuffix} §f[§a${player?.getSecrets()}§f]§r`
             }
-            return x;
+
+            if(/§5§o §\w\w+§f: §\w\w+§b \(§e\d+§b\)/.test(x)) {
+                const name = x.replace(/(.*§5§o §\w)|(§f: §\w\w+§b \(§e\d+§b\).*)/g, "")
+                Data.addPlayer(name)
+                hasChanged = true;
+                return `${x} §f[§a0§f]§r`
+            }
+
+            return x.replace(/§5§o/,"");
         })
-        item.setName(lore[0].replace("'s Party§r", "'s Party§f§r"))
-        item.setLore(lore)
+        if(hasChanged) {
+            item.setLore(lore)
+        }
+
     });
 }
 

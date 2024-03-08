0b4e090f91982df9aca506c5f64adc118538d108
Evankhell0 <68199237+Evankhell0@users.noreply.github.com>
2024-03-08 20:39:13 +0100

    refactor

--- features/roomtracker.js
+++ features/roomtracker.js
@@ -1,36 +1,55 @@
 import Config from "../Config.js";
+import Data from "../util/data.js";
+import Dungeon from "BloomCore/dungeons/Dungeon";
+
+const dungeonRooms = [];
 
 const registerWorldLoad = () => {
     register("worldLoad", () => {
-        let scoreboard = Scoreboard.getLines().map(a => { return ChatLib.removeFormatting(a) });
-        let lines = TabList.getNames().map(a => { return ChatLib.removeFormatting(a) });
-        for (let line of scoreboard) {
-            let match = line.match(/ ⏣ The Catac.+ombs \((.+)\)/);
-            if (match) {
-                trackRooms();
+        const scoreboard = Scoreboard.getLines().map(x => ChatLib.removeFormatting(x));
+        console.log(scoreboard.map(x => JSON.stringify(x)))
+        Data.inDungeon = scoreboard.some(x => /⏣ The Catac.+ombs \((.+)\)/.test(x))
+        console.log("SBD " + Data.inDungeon)
+    });
+
+    register("step", () => {
+        if(!Data.inDungeon)
+            return;
+        const scoreboard = Scoreboard.getLines().map(a => ChatLib.removeFormatting(a));
+        for(let line of scoreboard) {
+            const match = line.match(/^\d[\d/]+ .+ ([-\d,]+)$/);
+            if(match && (dungeonRooms.length === 0 || dungeonRooms[dungeonRooms.length - 1].roomname !== match[1])) {
+                dungeonRooms.push({
+                    timestamp: Date.now(),
+                    roomname: match[1]
+                });
             }
         }
-    });
-};
+    }).setFps(1);
 
-const trackRooms = () => {
-    register("tick", () => {
-        let dungeonRooms = [];
-        let scoreboard = Scoreboard.getLines().map(a => { return ChatLib.removeFormatting(a) });
-        for (let line of scoreboard) {
-            let match = line.match(/^\d[\d/]+ .+ ([-\d,]+)$/);
-            if (match) {
-                if (dungeonRooms.length === 0 || dungeonRooms[dungeonRooms.length - 1].roomname !== match[1]) {
-                    const obj = {
-                        timestamp: Date.now(),
-                        roomname: match[1]
-                    };
-                    dungeonRooms.push(obj);
-                   // console.log(dungeonRooms.map(x => JSON.stringify(x)))
-                }
-            }
+    register("chat", (score, rank) => {
+        let lastSlotItem = Player.getInventory().getStackInSlot(8);
+        if(lastSlotItem.getName().includes("Your Score Summary")) {
+            ChatLib.chat("Completed Dungeon");
+            combineTimes(dungeonRooms);
         }
-    });
+    }).setChatCriteria("Team Score: ${score} (${rank})").setContains();
+
+    register("command", (args) => {
+        console.log(dungeonRooms.map(x => JSON.stringify(x)))
+    }).setName("rooms")
 };
 
-module.exports = { registerWorldLoad, trackRooms };
+const combineTimes = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+        if(i - 1 == arr.length) {
+            arr[i].time = Date.now() - arr[i].timestamp
+        } else {
+            arr[i].time = arr[i+1].timestamp - arr[i].timestamp
+        }
+    }
+    console.log(arr)
+}
+
+
+module.exports = { registerWorldLoad };
--- util/data.js
+++ util/data.js
@@ -2,6 +2,7 @@ export default class Data {
     static players = {}
     static invalidKey = false
     static requests = []
+    static inDungeon = false
 
     // only allow 2 simultaneous calls to SkyCrypt API
     static staggerRequest(request) {

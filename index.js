24de3aa33917a2fa8059cda62b9b847ea7b0e918
Evankhell0 <68199237+Evankhell0@users.noreply.github.com>
2024-03-18 00:36:45 +0100

    remove old imports

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-import Data from "./util/data.js"
-import Config from "./Config.js"
 import { registerPartyFinderTriggers } from "./features/partyfinder.js"
 import { sbdCommand } from "./commands/sbdcommand.js"
 

e31eb0c25b8574dc002381372525afcaca01ffe0
Evankhell0 <68199237+Evankhell0@users.noreply.github.com>
2024-06-24 12:53:36 +0200

    prevent double join from breaking autokick

--- util/data.js
+++ util/data.js
@@ -1,6 +1,11 @@
 export default class Data {
     static players = {}
     static requests = []
+    static chatMessages = {
+        queue: [],
+        lastMessage: 0,
+        timeout: 400
+    }
 
     // only allow 2 simultaneous calls to SkyCrypt API
     static staggerRequest(request) {
@@ -20,5 +25,23 @@ export default class Data {
                 this.processNextRequest()
             }).catch(e => console.log(e))
         }
+    }
+
+    // prevent multiple messages/commands being sent at once and being blocked for spam
+    static staggerChatMessage(func) {
+        this.chatMessages.queue.push(func)
+        this.processNextChatMessage()
+    }
+
+    static processNextChatMessage() {
+        if(!this.chatMessages.queue.length) {
+            return
+        }
+        if(Date.now() - this.chatMessages.lastMessage >= this.chatMessages.timeout) {
+            this.chatMessages.lastMessage = Date.now()
+            const func = this.chatMessages.queue.shift()
+            func()
+        }
+        setTimeout(() => this.processNextChatMessage(), this.chatMessages.timeout)
     }
 }

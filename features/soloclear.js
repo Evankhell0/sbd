9b057cfc0f72a4609f97becc6e73f01c6eb13ea4
Evankhell0 <68199237+Evankhell0@users.noreply.github.com>
2025-03-07 12:26:08 +0100

    fix party detection, add hover details

--- features/soloclear.js
+++ features/soloclear.js
@@ -2,7 +2,7 @@ import Dungeon from "BloomCore/dungeons/Dungeon"
 import PogObject from "PogData"
 
 import Config from "../Config.js"
-import { timeToString } from "../util/calc.js"
+import { timeToString, yn } from "../util/calc.js"
 
 let finishedClear = false
 const pogData = new PogObject("sbd", {
@@ -15,9 +15,9 @@ export const registerSoloClearTriggers = () => {
             return
         }
         const dungeonScore = Dungeon.isPaul ? Dungeon.score - 10 : Dungeon.score
-        if(!finishedClear && dungeonScore >= 300 && Dungeon.partySize == 0) {
+        if(!finishedClear && dungeonScore >= 300 && isSoloClear()) {
             finishedClear = true
-            ChatLib.chat(`§8[§eSBD§8]§r Reached 300 score in §b${Dungeon.seconds}s§r (§e${Dungeon.floor}§r)`)
+            ChatLib.chat(formatEndOfRun(isPB()))
             saveSoloClear()
         }
     })
@@ -32,7 +32,7 @@ export const registerSoloClearTriggers = () => {
         ChatLib.chat(`§8[§eSBD§8]§r Top Solo Clears (§e${floorUpper}§r):`)
         for(let i = 0; i < amount; i++) {
             if(sorted[i]) {
-                ChatLib.chat(formatSoloClear(sorted[i], i+1))
+                formatSoloClear(sorted[i], i+1).chat()
             }
         }
     }).setName("topsoloclears")
@@ -46,7 +46,7 @@ export const registerSoloClearTriggers = () => {
         if(sorted.length) {
             for(let i = 1; i < 4; i++) {
                 const multi = Math.floor(sorted[0]?.seconds / 60) + i
-                ChatLib.chat(` §7➜§r Sub ${multi} Clears: §a${sorted.filter(x => x.seconds < 60 * multi).length}`)
+                ChatLib.chat(` §7➜§r Sub ${multi} Clears: §a${sorted.filter(x => x.seconds < 60 * multi && x.seconds >= 60 * (multi - 1)).length}`)
             }
         }
     }).setName("soloclearstats")
@@ -72,5 +72,37 @@ const saveSoloClear = () => {
 }
 
 const formatSoloClear = (data, rank = 0) => {
-    return `§8[§e#${rank}§8]§r §b${timeToString(data.seconds * 1000)}§r | §a${data.secretsFound}§r/§a${data.totalSecrets}§r | §d${data.completedPuzzles}P§r | §a${data.crypts}c§r`
+    const puzzleHover = data.puzzles.join("\n")
+    const extraHover = `Date: ${new Date(data.date).toLocaleDateString("en-US")}
+Mimic: ${yn(data.mimicKilled)}
+Paul: ${yn(data.isPaul)}`
+    const message = new Message(
+        `§8[§e#${rank}§8]§r §b${timeToString(data.seconds * 1000)}§r | §a${data.secretsFound}§r/§a${data.totalSecrets}§r | `,
+        new TextComponent(`§d${data.completedPuzzles}P§r`).setHover("show_text", `${puzzleHover}`),
+        `§r | §a${data.crypts}c§r | `,
+        new TextComponent(`§7Extra§r`).setHover("show_text", `${extraHover}`),
+    )
+    return message
+}
+
+const formatEndOfRun = (isPB) => {
+    return `§8[§eSBD§8]§r Reached 300 score in §b${timeToString(Dungeon.seconds * 1000)}§r (§e${Dungeon.floor}§r)${isPB ? " (§d§lPB§r)" : ""}`
+}
+
+const isSoloClear = () => {
+    const lines = Scoreboard.getLines()
+    for(let i = 0; i < lines.length; i++) {
+        if(/§3§lSolo/.test(lines[i])) {
+            return true
+        }
+    }
+    return false
+}
+
+const isPB = () => {
+    if(!pogData.soloClears[Dungeon.floor]) {
+        return true
+    }
+    const pb = Math.min(...pogData.soloClears[Dungeon.floor].map(x => x.seconds))
+    return Dungeon.seconds < pb
 }
